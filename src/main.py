8a359721eee3636942374cef1fa471cf8ffa16c8
Yaroslav <yarikbritov@yandex.ru>
2023-12-09 00:27:48 +0300

    include quote router

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
 from insults.insult import router as router_insult
+from quotes.quote import router as router_quote
 
 app = FastAPI(
-    title="FAST API_TG BOT",
+    title="FAST_API_TG_BOT",
 )
 
 app.include_router(router_insult)
+app.include_router(router_quote)
