bc9549d263c0d2418bb273499e920e6e8c94924f
Yaroslav <yarikbritov@yandex.ru>
2024-01-01 12:38:17 +0300

    create start models for simple behavioral users and statistics

---
+++ bot/models/models.py
@@ -0,0 +1,45 @@
+from sqlalchemy import MetaData, Table, Integer, Column, String, ForeignKey
+
+metadata = MetaData()
+
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("username", String, nullable=False, unique=True),
+)
+
+statistics = Table(
+    "statistics",
+    metadata,
+    Column(
+        "id",
+        Integer,
+        ForeignKey("users.id"),
+        primary_key=True,
+    ),
+    Column(
+        "username",
+        String,
+        ForeignKey("users.username"),
+        nullable=False,
+        unique=True,
+    ),
+    Column(
+        "matches",
+        Integer,
+        nullable=False,
+        default=0,
+    ),
+    Column(
+        "wins",
+        Integer,
+        nullable=False,
+        default=0,
+    ),
+    Column(
+        "matches",
+        Integer,
+        nullable=False,
+    ),
+)
